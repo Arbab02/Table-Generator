60918c83eba312e66f90a4c001b320b314e0b696
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2023-02-19 08:04:42 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+// let num = prompt("Enter the number");
+// let range = prompt("Enter the range");
+let box = document.getElementById('box');
+let h2 = document.getElementById('tabel');
+
+
+let num = document.getElementById('inp1');
+let range = document.getElementById('inp2');
+function fun() {
+
+  for (var input = 1; input <= range.value; input++) {
+    document.write("<h1>" + num.value + " x ", input + " = ", num.value * input, "<br>", "</h1>");
+    
+  }
+
+}
