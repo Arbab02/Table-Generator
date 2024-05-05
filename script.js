476a5814ccd9a92b9324ab7de4e95589bcd5e145
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-05-05 04:35:59 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
-
-
-// let num = prompt("Enter the number");
-// let range = prompt("Enter the range");
+// Initializing the Variables
 let box = document.getElementById('box');
-let h2 = document.getElementById('tabel');
-
-
+let text = document.getElementById('text');
 let num = document.getElementById('inp1');
 let range = document.getElementById('inp2');
+
+// Creating Function For clearing
+const cleared = () => {
+  text.value = "";
+}
+
+// Creating Function For result
 function fun() {
 
-  for (var input = 1; input <= range.value; input++) {
-    document.write("<h1>" + num.value + " x ", input + " = ", num.value * input, "<br>", "</h1>");
-    
-  }
+  text.value = '';
+  // Using For Loop For Printing Output
+  for (let input = 1; input <= range.value; input++) {
 
+   
+    text.value += "\n" + num.value + " x " + input + " = " + num.value * input;
+
+
+  }
+  num.value = "";
+  range.value = "";
 }
+  // let result = document.getElementById('result');
+  // result.innerHTML += "<br>" + num.value + " x " + input + " = " + num.value * input;
+
+  // let num = prompt("Enter the number");
+  // let range = prompt("Enter the range");
+
+  //   for (let input = 1; input <= range.value; input++) {
+  //     document.write("<center>" + "<h1>" + num.value + " x ", input + " = ", num.value * input, "<br>", "</h1>" + "</center>");
+
+  //   }
